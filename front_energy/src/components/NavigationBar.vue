d759a20e11a4990b2d260f4dbfa3783cb72b81d2
MollDoy <cema_007@mail.ru>
2026-03-29 01:35:02 +0800

    Renamed Price Analytics Page

--- front_energy/src/components/NavigationBar.vue
+++ front_energy/src/components/NavigationBar.vue
@@ -36,7 +36,7 @@ export default {
                 {to: "/information", name: "Информация"},
                 {to: "/table", name: "Таблица"},
                 {to: "/interactiveMap", name: "Карта"},
-                {to: "/price-analytics", name: "Цены по регионам"},
+                {to: "/price-analytics", name: "Аналитика цен"},
             ]
         }
     }
