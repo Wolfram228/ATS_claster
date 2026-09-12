fef416cba57c87f55b49c52714bd381120ec022c
Frizavi <darkmage5566@gmail.com>
2026-09-12 14:54:19 +0800

    auth_auto_check

--- front_energy/src/App.vue
+++ front_energy/src/App.vue
@@ -11,7 +11,34 @@
 <script>
 import Title from "./components/Title.vue"
 import NavigationBar from "./components/NavigationBar.vue";
+import { isAuth, checkAuth, clearTokens, getSavedUser } from "./utils/auth";
+import store from "./store";
+
 export default {
   components: { Title, NavigationBar },
+  async created() {
+    if (isAuth()) {
+      try {
+        const isValid = await checkAuth();
+        
+        if (!isValid) {
+          clearTokens();
+          store.commit("SET_AUTHENTICATED", false);
+          store.commit("SET_USER", null);
+          if (this.$route.path !== "/login" && this.$route.path !== "/register") {
+            this.$router.push("/login");
+          }
+        } else {
+          const user = getSavedUser();
+          if (user) {
+            store.commit("SET_USER", user);
+            store.commit("SET_AUTHENTICATED", true);
+          }
+        }
+      } catch (error) {
+        console.error("Auth check error:", error);
+      }
+    }
+  }
 }
 </script>
